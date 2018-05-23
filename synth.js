bb828e7b568d4c8ba7dee336797f714c7af83ea5
Shreyas Daniel <shreyasdaniel2001@yahoo.co.in>
2018-05-23 20:35:49 +0530

    removed silly console logs

--- synth.js
+++ synth.js
@@ -1,31 +1,26 @@
 function playNote(e) {
-    
     keyCode = e.keyCode;
     keyCode = 'key_'.concat(parseInt(keyCode));
     const note = document.getElementById(keyCode);
     var key = document.getElementsByClassName(keyCode)[0];
     key.classList.add("pressed");
     if(!note) {return;}
-    console.log("note");
-    console.log(key);
     note.currentTime=0;
-    note.play();
-    
+    note.play();   
 }
 
-function stopNote(e) {
-    
+
+function stopNote(e) { 
     keyCode = e.keyCode;
     keyCode = 'key_'.concat(parseInt(keyCode));
     const note = document.getElementById(keyCode);
     var key = document.getElementsByClassName(keyCode)[0];
     key.classList.remove("pressed");
     if(!note) {return;}
-    console.log("stop");
     note.currentTime = 0;
     note.pause();
-
 }
+
 
 function howto() {
     window.alert("q, w, e, r, t, y, u, i, o, p, [, ],\\ <|== notes")
